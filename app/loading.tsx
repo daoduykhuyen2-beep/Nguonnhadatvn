002a618c4dd303abe635ee2ac07062f2d5b585c4
Đào Duy Khuyến <daoduykhuyen2@gmail.com>
2026-07-16 00:41:11 +0700

    feat: app/loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="container-app py-24">
+      <div className="mx-auto h-10 w-10 animate-spin rounded-full border-4 border-brand-100 border-t-brand-600" />
+    </div>
+  );
+}
