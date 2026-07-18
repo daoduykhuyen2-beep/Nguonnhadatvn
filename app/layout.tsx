2b7145f24bf3c7119cb027291fc8fc34f2e83424
Đào Duy Khuyến <daoduykhuyen2@gmail.com>
2026-07-18 20:54:00 +0700

    Fix Google Analytics script syntax in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,16 +42,16 @@ export default function RootLayout({
         <Footer />
         <VisitTracker />
         <Script
-          src={\`https://www.googletagmanager.com/gtag/js?id=\${GA_MEASUREMENT_ID}\`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
-          {\`
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '\${GA_MEASUREMENT_ID}');
-          \`}
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
         </Script>
       </body>
     </html>
